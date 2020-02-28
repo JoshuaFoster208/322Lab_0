356e671ff8f84510a5ebc2d76c0900c04f6ef15a
joshua.foster208 <ffcleader@gmail.com>
2020-02-28 01:10:18 +0000

    Updated the files so that it runs on linux with new makefile

--- Lab1/Lab1/main.c
+++ Lab1/Lab1/main.c
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: josht
- *
- * Created on February 22, 2020, 1:41 PM
- */
+//Header Files
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,51 +8,40 @@
 #include <sys/wait.h>
 #include <sys/times.h>
 
-
-/*
- * 
- */
-
-time_t printStartTime()
-{
+//prints the first lines and the start
+time_t printStartTime(){
     time_t T = time(NULL);
     printf("START: %ld\n", T);
     return T;
 }
 
-void splitProcess(time_t T)
-{
-int pid, status = 0; 
-pid_t wait;
-pid = fork();
-wait = waitpid(pid, status, 0);
-printProcess(pid, status, T);
+//prints processes for both the child and parent fucntions
+void printProcess(int P, int S){
+    struct tms buf;
+    printf("PPID: %d, ", getppid());
+    if(P == 0){
+    printf("PID: %d\n", getpid());
+    }
+    else{
+    printf("PID: %d, CPID: %d, RETVAL: %d\n", getpid(), P, S);
+    printf("USER: %lu, SYS: %lu\n", buf.tms_utime, buf.tms_stime);
+    printf("CUSER: %lu, CSYS: %lu\n", buf.tms_cutime, buf.tms_cstime);
+    printf("STOP: %lu\n", time(NULL));
+    }
 }
 
-void printProcess(int P, int S, time_t sTime)
-{
-time_t cTime, pTime;
-struct tms buf;
-printf("PPID: %d, ", getppid());
-if(P == 0)
-{
-printf("PID: %d\n", getpid());
-cTime = time(NULL) - sTime;
-}
-else
-{
-
-printf("PID: %d, CPID: %d, RETVAL: %d\n", getpid(), P, S);
-pTime = time(NULL) - sTime;
-printf("USER: %jd, SYS: %jd\n", buf.tms_utime, buf.tms_stime);
-printf("CUSER: %jd, CSYS: %jd\n", buf.tms_cutime, buf.tms_cstime);
-printf("STOP: %jd\n", time(NULL));
-}
+void splitProcess(){
+    int pid;
+    int status = 0; 
+    pid_t wait;
+    pid = fork();
+    wait = waitpid(pid, &status, 0);
+    clock_t times(struct tms *buf);
+    printProcess(pid, status);
 }
 
 int main(int argc, char** argv) {
     time_t T;
-    clock_t times(struct tms *buf);
     T = printStartTime();
     splitProcess(T);
    // printf("%d\n", startTime); 
