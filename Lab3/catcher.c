f74cedb0b2e5cad3293aa044219b9ec534924c65
joshua.foster.208 <ffcleader@gmail.com>
2020-03-23 15:31:45 -0700

    Added comments and an error check method

--- Lab3/catcher.c
+++ Lab3/catcher.c
@@ -6,20 +6,14 @@
 #include <unistd.h>
 #include <string.h>
 
-/*
- * 
- */
-int count = 0;
-int terms = 0;
-static char* sigArray[] = {"SIGHUP", "SIGINT", "SIGQUIT", "SIGILL", "SIGTRAP", "SIGABRT", "SIGEMT",
-"SIGFPE", "SIGKILL", "SIGNUS", "SIGSEGV", "SIGSYS", "SIGPIPE", "SIGALRM", "SIGTERM",
-"SIGURG", "SIGSTOP", "SIGTSTP", "SIGCONT", "SIGCHLD", "SIGTTIN", "SIGTTOU", "SIGIO", "SIGXCPU",
-"SIGXFSZ", "SIGVTALRM", "SIGPROF", "SIGWINCH", "SIGINFO", "SIGUSR1", "SIGUSR2"}; 
-static const char sigArray1[31][16] = {"HUP", "INT", "QUIT", "ILL", "TRAP", "ABRT", "BUS", "FPE", "KILL",
+/*Global Variables*/
+static int count = 0;
+static int terms = 0;
+static const char* sigArray[] = {"HUP", "INT", "QUIT", "ILL", "TRAP", "ABRT", "BUS", "FPE", "KILL",
                                   "USR1", "EGV", "USR2", "PIPE", "ALRM", "TERM", "CHLD", "CONT", "STOP",
                                   "TSTP", "TTIN", "TTOU", "URG", "XCPU", "XFSZ", "VTALRM", "PROF",
                                   "WINCH", "IO", "PWR", "SYS"};  
-
+/*NonMain functions*/
 void printStatusLine();
 void sigHandler(int sig); 
 void initSignal(int argc, char** argv);
@@ -27,7 +21,7 @@ void pauseLoop(int argc, char** argv);
 void finish();
 int retrvNumber(char str[]);
 
-
+/*EntryPoint for the program*/
 int main(int argc, char** argv) {
     printStatusLine();
     initSignal(argc, argv);
@@ -42,8 +36,10 @@ void printStatusLine(){
     fprintf(stderr, "catcher: $$ = %d\n", p);
 }
 
+/*Signal handler tracks catches and tracks total signals 
+  also tracks SIGTERMS in a row*/
 void sigHandler(int sig){
-    printf("%s caught at %ld\n", sigArray[sig-1], time(NULL));
+    printf("SIG%s caught at %ld\n", sigArray[sig-1], time(NULL));
     if(sig == 15)
         terms++;
     else
@@ -51,16 +47,19 @@ void sigHandler(int sig){
     count++;
 }
 
+/*loops through and calls signal on all parameters */
 void initSignal(int argc, char** argv){
     int i;
     for(i = 1; i < argc; i++){
-    /*if(signal(retrvNumber(temp), sigHandler) == SIG_ERR)
-    printf("can't catch sig\n");*/
-        signal(retrvNumber(argv[i]), sigHandler);
-    }
+    if((signal(retrvNumber(argv[i]), sigHandler), sigHandler) == SIG_ERR){
+        printf("can't catch sig\n");
 
+        }
+    }
 }
 
+/*pauses the process until next signal called 
+ also reinitializes the signals after the call*/
 void pauseLoop(int argc,char** argv){
     while(terms != 3){
         pause();
@@ -68,16 +67,17 @@ void pauseLoop(int argc,char** argv){
     }       
 }
 
+/*Prints signal total and gracefully exits program*/
 void finish(){
     fprintf(stderr, "catcher: Total signals count = %d\n", count); 
     exit(EXIT_SUCCESS);
 }
 
+/*retrieves the number corresponding to the signal from sigArray and returns it*/
 int retrvNumber(char str[]){
     int i = 0;
-    while(i < 31 && (strcmp(sigArray1[i], str) != 0)){
+    while(i < 31 && (strcmp(sigArray[i], str) != 0)){
         i++;        
     }
-//    printf("return %d\n", i+1);
     return ++i;
 }
