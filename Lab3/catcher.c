da78289b735ca6b6fbd669ebf13de3704cd481db
joshua.foster.208 <ffcleader@gmail.com>
2020-03-22 21:19:39 -0700

    Basics finished begining testing

--- Lab3/catcher.c
+++ Lab3/catcher.c
@@ -1,24 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   catcher.c
- * Author: josht
- *
- * Created on March 19, 2020, 10:24 PM
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <wait.h> 
+#include <time.h>
+#include <unistd.h>
 
 /*
  * 
  */
-int main(int argc, char** argv) {
+static int Count = 0;
+static char* sigArray = {"SIGHUP", "SIGINT", "SIGQUIT", "SIGILL", "SIGTRAP", "SIGABRT", "SIGEMT"
+"SIGEMT","SIGFPE", "SIGKILL", "SIGNUS", "SIGSEGV", "SIGSYS", "SIGPIPE", "SIGALRM", "SIGTERM",
+"SIGURG", "SIGSTOP", "SIGTSTP", "SIGCONT", "SIGCHLD", "SIGTTIN", "SIGTTOU", "SIGIO", "SIGXCPU",
+"SIGXFSZ", "SIGVTALRM", "SIGPROF", "SIGWINCH", "SIGINFO", "SIGUSR1", "SIGUSR2"}; 
 
-    return (EXIT_SUCCESS);
+
+void printStatusLine();
+void sigHandler(int sig); 
+void initSignal(int argc, char* argv);
+void pauseLoop();
+
+int main(int argc, char* argv) {
+    printStatusLine();
+    initSignal(argc, argv);
+    return (0);
 }
 
+/*Prints initial status and process ID*/
+void printStatusLine(){
+    pid_t p = getpid();
+    fprintf(stderr, "catcher: $$ = %d\n", p);
+}
+
+void sigHandler(int sig){
+    fprintf(stdout, "%s caught at %d\n", sigArray[sig-1], time(NULL));
+    Count++;
+}
+
+void initSignal(int argc, char* argv){
+    int i;
+    for(i = 1; i < argc; i++){
+    if(signal("SIG"+argv[1], sigHandler) == SIG_ERR)
+    printf("can't catch sig\n");
+    }
+}
+
+void pauseLoop(){
+    while(1){
+        pause();
+    }
+        
+}
