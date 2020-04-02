7a809c9ae9627c3035e2f8d2228cd666c623610a
joshua.foster.208 <ffcleader@gmail.com>
2020-04-02 14:31:34 -0700

    Setup framework of lab4

---
+++ Lab4/dining-p.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void eat();
+void think();
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
+void eat(){
+    
+}
+
+void think(){
+    
+}
