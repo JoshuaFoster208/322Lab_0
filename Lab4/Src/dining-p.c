a97f524dc358feeae288a6ae46512ee0f590c3f6
joshua.foster.208 <ffcleader@gmail.com>
2020-04-20 22:21:21 -0700

    Converted back to named semaphores, added struct for the chopsticks and a new function to initialize that struct

--- Lab4/Src/dining-p.c
+++ Lab4/Src/dining-p.c
@@ -8,27 +8,38 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 #include <signal.h>
 
-
+#define pGroupFile "PGROUP.txt"
 sem_t * returnVal;
 static int fullCycles = 0;
 static int Finished = 0;
 
+struct chpInfo {
+    int seat;
+    int pos;
+    sem_t **chopstick;
+};
+
 void validParams(int argc, char** argv);
-void eat(int p);
-void think(int p);
+void eat(struct chpInfo *ch);
+void think(struct chpInfo *ch);
 void randTime();
-void dining(int argc, char** argv);
+void dining(int argc, char** argv, struct chpInfo *ch);
 void sigHandler(int sig);
-void initSem(int seats, sem_t *S);
+void initSem(char **argv, struct chpInfo *ch);
 void initGpid();
 void finishEating();
+void sigSet();
 
 int main(int argc, char** argv) {
+    struct chpInfo I; 
     validParams(argc, argv);
+    sigSet();
+    initGpid();
     fprintf(stdout,"Pid: %d\n", getpid());
-    dining(argc, argv);
+    dining(argc, argv, &I);
     printf("pid: %d \npgid: %d\n", getppid(),getpgid(0));
     return (EXIT_SUCCESS);
 }
@@ -52,9 +63,23 @@ void validParams(int argc, char** argv){
     }
 }
 
+void sigSet(){
+    if(signal(SIGTERM, sigHandler) == SIG_ERR){
+        fprintf(stderr, "Error: Could not register signal!\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
 /*Begin Dining*/
-void dining(int argc, char** argv){
-    int seats = atoi(argv[1]);
+void dining(int argc, char** argv, struct chpInfo *ch){
+    initSem(argv, ch);
+    do{
+    eat(ch);
+    think(ch);
+    fullCycles++;
+    }while(!Finished);
+            
+   /* int seats = atoi(argv[1]);
     int pos = atoi(argv[2]);
     int i;
     int fd = shm_open(argv[2], O_RDWR | O_CREAT, 0666);
@@ -73,54 +98,73 @@ void dining(int argc, char** argv){
         fprintf(stderr, "Error: Failed to mmap() the memory object\n");
 	exit(EXIT_FAILURE);
     }
-    /*After finishing mapping you can unlink*/
+    //After finishing mapping you can unlink
     if (shm_unlink(argv[2]) < 0) {
         fprintf(stderr, "Error: failed to unlink the memory object\n");
     }
 
     initGpid();
-    signal(SIGTERM, sigHandler);
     for(i = 0; i < seats; i++){	
             if(sem_init(chopsticks[i], 1, 1) != 0){
                     fprintf(stderr,"Error: Failed to initialize semaphore\n");
             }
     }
-    do{
-        /*sem_wait(chopsticks[pos]);
-        sem_wait(chopsticks[(pos+1) % seats]);*/
-        eat(pos);
-        /*sem_post(chopsticks[pos]);
-        sem_post(chopsticks[(pos+1) % seats]);*/
-        think(pos);
-        fullCycles++;
-    }while(!Finished);
+    */
 }
 
 /*Philosopher "eats" for random time*/
-void eat(int p){
+void eat(struct chpInfo *ch){
     randTime();
-    fprintf(stdout, "Philosopher #%d is eating\n", p);
+    fprintf(stdout, "Philosopher #%d is eating\n", ch->pos);
 }
 
 /*Philosopher "thinks" for random time*/
-void think(int p){
+void think(struct chpInfo *ch){
     randTime();
-    fprintf(stdout, "Philosopher #%d is thinking\n", p);
+    fprintf(stdout, "Philosopher #%d is thinking\n", ch->pos);
 }
 
-void initSem(int seats, sem_t *S){
-    int semCount = 1;
-    char semNames[12];
-    
-    // 0666 = rw-rw-rw
-    for(semCount = 1; semCount < seats+1; semCount++){
-    sprintf(semNames, "%d", semCount);
-    S = sem_open(semNames, O_CREAT|O_EXCL, 0666, 1);
-    }
+void initSem(char **argv, struct chpInfo *ch){
+    int i = 0;
+    char chops[40];
+        ch->seat = atoi(argv[1]);
+        ch->pos = atoi(argv[2]);
+        ch->chopstick = malloc(sizeof(sem_t*) *ch->seat);
+        for (i = 1; i < ch->seat; i++) {
+            //fill array/buffer before copying chopstick number into it
+            memset(chops,0,40);
+            sprintf(chops, "Chopstick%d", i);
+            ch->chopstick[i] = sem_open(chops, O_CREAT, 0666, 1);
+
+        }
 }
 
 void initGpid(){
-    setpgid(getpgrp(),0);
+   FILE *pnt ; 
+   struct stat buffer;
+    
+    /* Check if file PGROUP.txt currently exists */
+    if (stat(pGroupFile, &buffer) == 0){
+        //buffer for getline
+        char *b = NULL;
+        size_t size = 0;
+        //PGROUP.txt already exists open it to be read with "r"
+        pnt = fopen(pGroupFile, "r");
+        getline(&b, &size, pnt);
+        setpgid(getpid(), strtol(b, NULL, 10));
+        fclose(pnt);
+    }
+    else {
+        int gpid = getpgid(getpid());
+        //20 covers size of process IDS
+        char pid[20];
+        memset(pid, 0, 20);
+        //PGROUP.txt doesnt exist create it and open to write with "w"
+        pnt = fopen(pGroupFile, "w");
+        sprintf(pid, "%d", gpid);
+        fwrite(pid, sizeof(char), sizeof(pid), pnt);
+        fclose(pnt);
+    }
 }
 
 void sigHandler(int sig){
@@ -142,3 +186,4 @@ void randTime(){
         exit(EXIT_FAILURE);
     }
 }
+
