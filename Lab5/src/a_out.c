4cab28aaed31dadd01bd22a44bd3403b9ba888a4
joshua.foster.208 <ffcleader@gmail.com>
2020-05-02 17:58:57 -0700

    Modified the make file and fixed the pass by pointer

--- Lab5/src/a_out.c
+++ Lab5/src/a_out.c
@@ -4,11 +4,14 @@
 /*
  * 
  */
-void calcInfo(unsigned long int address, unsigned int *offset, 
-              unsigned int *pageNum); 
+void calcInfo(unsigned long int address, unsigned long int *offset, 
+              unsigned long int *pageNum); 
+void validParams(int argc);
+void output(char** argv);
 
 int main(int argc, char** argv) {
-
+    validParams(argc);
+    output(argv);
     return (EXIT_SUCCESS);
 }
 
@@ -16,6 +19,27 @@ int main(int argc, char** argv) {
 Uses unsigned long int as it must be a 32 bit datatype*/
 void calcInfo(unsigned long int address, unsigned long int *offset, 
               unsigned long int *pageNum){
-    offset = address % 4096;
-    pageNum = offset / 4096;
+    *offset = address % 4096;
+   *pageNum = address / 4096;
+}
+
+void validParams(int argc){
+    if(argc < 2){
+        fprintf(stderr, "Error: Not enough arguments\n");
+        fprintf(stderr, "Only 1 argument needed: pass in address to calculate.\n");
+        exit(EXIT_FAILURE);
+    } else if(argc > 3){
+        fprintf(stderr, "Error: Too many arguments\n ");
+        fprintf(stderr, "Only 1 argument needed: pass in address to calculate.\n");
+        exit(EXIT_FAILURE);        
+    }
+}
+
+void output(char** argv){
+    unsigned long int add, off, page;
+    add = atoi(argv[1]);
+    calcInfo(add, &off, &page);
+    fprintf(stdout, "The address %lu contains:\n", add);
+    fprintf(stdout, "page number = %lu\n", page);
+    fprintf(stdout, "offset = %lu\n", off);
 }
