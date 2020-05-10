4b987c6ea01b6217aeadbeaea693ebb44282a5a5
joshua.foster.208 <ffcleader@gmail.com>
2020-05-10 01:04:20 -0700

    finished and tested, many changes and fixes

--- lab7/src/matrix_add.c
+++ lab7/src/matrix_add.c
@@ -12,113 +12,101 @@
 
 #define IN STDIN_FILENO
 #define OUT STDOUT_FILENO
-
+int count=0;
 /*I gotta say the linux manual page helped with this project more than any of 
  the others besides maybe lab4*/
 
-/* for reference
- * http://man7.org/linux/man-pages/man7/aio.7.html
- * struct aiocb {
-     The order of these fields is implementation-dependent 
-
-   int             aio_fildes;     // File descriptor 
-    off_t           aio_offset;     // File offset 
-    volatile void  *aio_buf;        // Location of buffer 
-    size_t          aio_nbytes;     // Length of transfer 
-    int             aio_reqprio;    // Request priority 
-    struct sigevent aio_sigevent;   // Notification method 
-    int             aio_lio_opcode; // Operation to be performed;
-                                       lio_listio() only 
-   /* Various implementation-internal fields not shown 
-};*/
-
 void run(int argc, char **argv);
-void matrix_add(int size, int blocks, int scalar, struct aiocb *data);
-void setIn(int offset, int blockSize, struct aiocb *data);
-void setOut(int offset, int blockSize, struct aiocb *data);
+void matrix_add(int size, int scalar, struct aiocb *data);
+void setIn(off_t offset, int displacement, struct aiocb *data);
+void setOut(off_t offset, struct aiocb *data);
+void outputTime(int sTime);
+
 int main(int argc, char **argv) {
     run(argc, argv);
-
     return (EXIT_SUCCESS);
 }
 
 void run(int argc, char **argv){
     argc = argc;
-    int startTime, scalar, size, blocks, blockSize, offset; 
+    int startTime, scalar, size, blocks, blockSize, offset, displacement; 
     char *buf;
     struct aiocb last, current, next;
     
     /*Setup necessary variables*/
     startTime = time(NULL);
-    printf("%d", startTime);
+    srand(time(NULL));//changes the random seed
+
+    //printf("%d\n", startTime);
     size = strtol(argv[1], &buf, 10);
     blocks = strtol(argv[2], &buf, 10);    
-    scalar = rand() % 101;
-    scalar = scalar;
+    scalar = rand() % 101;//0-100 same as the one in matrix_gen
+    displacement = 0;
     blockSize = size / blocks;
+//verify if parameters divide evenly        
      if (size % blocks) {
         fprintf(stderr, "Error: Invalid Entries(line 60)\n");
         exit(EXIT_FAILURE); 
-    }
+    }    
+    //4 bytes per col based on max size of value and ^2 for the part 2 for loop
+    offset = (blockSize*4);
+    offset = offset*offset;
     
-    
-    //4 bytes per col based on max size of value
-    offset = (blockSize*4)*2;
+    setIn(offset, displacement, &current);
 
+    //call aio_read after the setup
+    aio_read(&current);   
     
-    setIn(offset, blockSize, &current);
-   
-    //call aoi_read after the setup
-    aio_read(&current);    
     //wait for read to finish
     //cant use suspendaio_suspend(&first, ,NULL);
     while(aio_error(&current) == EINPROGRESS){
-    };
-
-    //retrieve the aiocb information now that the i/o op is complete
+    }
+    //retrieve the aiocb completion now that the i/o op is complete
     aio_return(&current);
+    //loop throught the full size of the new space
+     for (displacement = offset; displacement < (size*size)*4; 
+             displacement = displacement + offset) {
        
-     for (int i = offset; i < (size*4)*2; i += offset) {
-    /*repeat steps above reminder come back and make it a function if this works*/
-    
-    setIn(offset, blockSize, &next);
+    /*repeat steps to setup read from before reminder come back and make it a 
+     * function if this works: it did work added setIn and setOut*/
+    setIn(offset, displacement, &next);
   
-        /* Execute read on the next block */
-        aio_read(&next);
+    //call read on the next struct
+    aio_read(&next);
         
-        /* See busy wait justification above in first call to aio_error() */
-        while(aio_error(&next) == EINPROGRESS){
-        };
-
-        /* Add scalar to last read block */
-    matrix_add(size, blocks, scalar, &current);
-
-        /*move the scalared values from current to last so that last can 
-         * write the info*/
-        memcpy(&last, &current, sizeof(struct aiocb));
-
-        // sets up last for a write call this time  
-        setOut(offset, blockSize, &last);
-        
-        aio_write(&last);
-        
-        //wait again but for the write call
-        while (aio_error(&last) == EINPROGRESS){
-        };
-        aio_return(&last);
-        /* Sync the write output to disk */
-        aio_fsync(O_SYNC, &last);
-
-        /* Overwrite the current block that has already been copied to 'last' and
-         * written to stdout, with the next block prepped for aio_reads */
-        memcpy(&current, &next, sizeof(struct aiocb));
+    //wait for read
+    while(aio_error(&next) == EINPROGRESS){
     }
 
-    /* Last drain the pump by handling the last block */
-    matrix_add(size, blocks, scalar, &current);
+    //add the scalar
+    matrix_add(size, scalar, &current);
+    /*move the scalared values from current to last*/
+    memcpy(&last, &current, sizeof(struct aiocb));
 
-    /* Write the last 'current' block to stdout */
-    setOut(offset, blockSize, &last);
+    // sets up last for a write call this time  
+    setOut(offset, &last);
+
+    aio_write(&last);
+
+    //wait again but for the write call
+    while (aio_error(&last) == EINPROGRESS){
+    }
+    aio_return(&last);
+        
+    //it is only a request so you have to check if it failed to sync
+    if(aio_fsync(O_SYNC, &last) == -1){
+        fprintf(stderr,"Error: last failed to sync (line 156)\n");
+    }
+
+    /* now that current is free copy over the next struct
+     *  might switch to safer memmove*/
+    memcpy(&current, &next, sizeof(struct aiocb));
+    }
+
+    //do the last struct
+    matrix_add(size, scalar, &current);
+//finish the current struct
+    setOut(offset, &current);
     aio_write(&current);
     while (aio_error(&current) == EINPROGRESS){};
     aio_return(&current);
@@ -126,68 +114,74 @@ void run(int argc, char **argv){
     if(aio_fsync(O_SYNC, &current) == -1){
         fprintf(stderr,"Error: current failed to sync (line 156)\n");
     }
-
+    outputTime(startTime);
 }
 
-void matrix_add(int size, int blocks, int scalar, struct aiocb *data){
+void matrix_add(int size, int scalar, struct aiocb *data){
     //unsigned for negative values
-    unsigned holdNum;
+    unsigned int holdNum;
     //holds the 4 bytes plus the '\0' 
-    char* buf1[5], buf2[5];
+    char buf[5];
     /* loop thorugh the buffer in data while i < number of bytes
      * increment by 4 to account for the size
      * switch to unsigned int i to cover comparison issues*/
     for (unsigned int i = 0; i < data->aio_nbytes; i = i + 4) {
-        /*from what I read online memset may be overkill: reset the string 
-         * so that the following numbers copied dont overlap
-         * such as in the case of the first number being -100 
-         * and the second being 1 you would get -101*/
-        memset(buf1, '\0', sizeof(buf1));
-        memset(buf2, '\0', sizeof(buf2));
+/*from what I read online memset may be overkill: reset the string so that the
+ *  following numbers copied dont overlap such as in the case of the first 
+ * number being -100 and the second being 1 you would get -101*/
+        memset(buf, '\0', sizeof(buf));
 
-        /* move 4 bytes from the buffer
-         * add i to the address it as it loops to get the next number*/
-        memmove(buf1, (void *)data->aio_buf+i, 4);
-        
-        holdNum = atoi((void*)buf1) + scalar;
-
-        sprintf(buf2, "%4d", holdNum);
-
-        // replace the value in the buffer
-        memmove((void *)data->aio_buf+i, buf2, 4);
-        
-        
-        
+/* move 4 bytes from the buffer
+ * add i to the address as it loops to get the next number*/
+        memmove(buf, (void *)data->aio_buf+i, 4);
+        holdNum = strtol(buf, NULL, 10) + scalar;
+//print to the file
+        fprintf(stdout, "%4d", holdNum);
+//increment count if count %size is 0 skip line 
+        count++;
+        if(count%size == 0){
+            fprintf(stdout, "\n");
+        }
     }
 }
 
-void setIn(int offset, int blockSize, struct aiocb *data){
+/*sets up the aiocb for aioread calls*/
+void setIn(off_t offset, int displacement, struct aiocb *data){
   //zero out before the aioreadrequest so that nothing can interupt it
-    memset(&data, 0, sizeof(struct aiocb));
+    memset(data, 0, sizeof(struct aiocb));
     //set the fd to in for read
     data->aio_fildes = IN;
     //set the bytes per block size
-    data->aio_nbytes = blockSize;
+    data->aio_nbytes = offset;
     //set the offset
-    data->aio_offset = offset;
+    data->aio_offset = displacement;
     /*missed these at first 
      * need to allocate space for read operations
-     * have to set a priority even if said priority wont matter in this example
+     * have to set a priority 
      * */
-    data->aio_buf = malloc(blockSize);
+    data->aio_buf = malloc(offset);
+    /*not sure if necessary but all examples I've seen have had them
+     * to be safe set all to 0*/
     data->aio_reqprio = 0;   
+
 }
 
-void setOut(int offset, int blockSize, struct aiocb *data){
-/*a classmate told me that i shouldnt be zeroing out stdout, 
+/*sets up the aiocb for aiowrite calls*/
+void setOut(off_t offset, struct aiocb *data){
+/*a classmate told me that I shouldnt be zeroing out stdout, 
  * I'm gonna look into why but for now it works*/
     //set the fd to in for read
     data->aio_fildes = OUT;
     //set the bytes per block size
-    data->aio_nbytes = blockSize;
+    data->aio_nbytes = offset;
     //set the offset
-    data->aio_offset = offset;
+    data->aio_offset = 0;
     /*missed this at first 
-     * have to set a priority even if said priority wont matter in this example*/
+     * have to set a priority */
     data->aio_reqprio = 0;   
+}
+
+/*outputs the start and endtimes to the end of the file*/
+void outputTime(int sTime){
+    fprintf(stdout, "\nstart time %d - end time %ld\n", sTime, time(NULL));
 }
