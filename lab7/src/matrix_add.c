a0d1d221d67784967a58d230867602c46317f9aa
joshua.foster.208 <ffcleader@gmail.com>
2020-05-10 15:20:10 -0700

    Added several more explanetary comments and made it so that scalar can be posiitive or negative, also testing to see if clones change after pushing

--- lab7/src/matrix_add.c
+++ lab7/src/matrix_add.c
@@ -40,7 +40,10 @@ void run(int argc, char **argv){
     //printf("%d\n", startTime);
     size = strtol(argv[1], &buf, 10);
     blocks = strtol(argv[2], &buf, 10);    
-    scalar = rand() % 101;//0-100 same as the one in matrix_gen
+    scalar = rand() % 101;/*0-100 makes it so that it should have a mix of 
+    * positive and negative numbers after the addition*/
+    if(time(NULL) % 2)
+        scalar = scalar * -1;// if rand is odd make negative scalar
     displacement = 0;
     blockSize = size / blocks;
 //verify if parameters divide evenly        
@@ -58,7 +61,7 @@ void run(int argc, char **argv){
     aio_read(&current);   
     
     //wait for read to finish
-    //cant use suspendaio_suspend(&first, ,NULL);
+    //cant use suspend aio_suspend(&first, ,NULL); so switched to while
     while(aio_error(&current) == EINPROGRESS){
     }
     //retrieve the aiocb completion now that the i/o op is complete
@@ -74,7 +77,8 @@ void run(int argc, char **argv){
     //call read on the next struct
     aio_read(&next);
         
-    //wait for read
+    /*wait for read I've determined it can work without this but this is a good 
+    practice to have so I kept it in*/
     while(aio_error(&next) == EINPROGRESS){
     }
 
@@ -95,7 +99,7 @@ void run(int argc, char **argv){
         
     //it is only a request so you have to check if it failed to sync
     if(aio_fsync(O_SYNC, &last) == -1){
-        fprintf(stderr,"Error: last failed to sync (line 156)\n");
+        fprintf(stderr,"Error: last failed to sync (line 99)\n");
     }
 
     /* now that current is free copy over the next struct
@@ -105,14 +109,14 @@ void run(int argc, char **argv){
 
     //do the last struct
     matrix_add(size, scalar, &current);
-//finish the current struct
+    //finish the current struct
     setOut(offset, &current);
     aio_write(&current);
     while (aio_error(&current) == EINPROGRESS){};
     aio_return(&current);
     //only a request have to check if it failed to sync
     if(aio_fsync(O_SYNC, &current) == -1){
-        fprintf(stderr,"Error: current failed to sync (line 156)\n");
+        fprintf(stderr,"Error: current failed to sync (line 116)\n");
     }
     outputTime(startTime);
 }
@@ -122,25 +126,29 @@ void matrix_add(int size, int scalar, struct aiocb *data){
     unsigned int holdNum;
     //holds the 4 bytes plus the '\0' 
     char buf[5];
-    /* loop thorugh the buffer in data while i < number of bytes
+    /* loop through the buffer in data while i < number of bytes
      * increment by 4 to account for the size
      * switch to unsigned int i to cover comparison issues*/
     for (unsigned int i = 0; i < data->aio_nbytes; i = i + 4) {
-/*from what I read online memset may be overkill: reset the string so that the
- *  following numbers copied dont overlap such as in the case of the first 
- * number being -100 and the second being 1 you would get -101*/
-        memset(buf, '\0', sizeof(buf));
-
-/* move 4 bytes from the buffer
- * add i to the address as it loops to get the next number*/
-        memmove(buf, (void *)data->aio_buf+i, 4);
-        holdNum = strtol(buf, NULL, 10) + scalar;
-//print to the file
-        fprintf(stdout, "%4d", holdNum);
-//increment count if count %size is 0 skip line 
+        // increment the counter that stands for total number of loops
         count++;
+/*cant just reset the first value the way you would normally reset strings in c:
+ *  reset the whole string so that the following numbers copied dont overlap 
+ * such as in the case of the first number being -100 and the second being 1 
+ * you would get -101*/
+        memset(buf, '\0', sizeof(buf));
+/* move 4 bytes from the buffer
+ * add i to the address as it loops to get the next number
+ * reminder to look up what this technique was called in assembly*/
+        memmove(buf, (void *)data->aio_buf+i, 4);
+        //atoi couldnt match types passed in, strtol neccesary
+        holdNum = strtol(buf, NULL, 10) + scalar;
+        //print to the file
+        fprintf(stdout, "%4d", holdNum);
+        //increment count if count %size is 0 skip line 
+        //whenever count %size is 0 output a line space to format the file 
         if(count%size == 0){
-            fprintf(stdout, "\n");
+           fprintf(stdout, "\n");
         }
     }
 }
@@ -149,7 +157,7 @@ void matrix_add(int size, int scalar, struct aiocb *data){
 void setIn(off_t offset, int displacement, struct aiocb *data){
   //zero out before the aioreadrequest so that nothing can interupt it
     memset(data, 0, sizeof(struct aiocb));
-    //set the fd to in for read
+    //set the fd to IN macro for read
     data->aio_fildes = IN;
     //set the bytes per block size
     data->aio_nbytes = offset;
@@ -170,7 +178,7 @@ void setIn(off_t offset, int displacement, struct aiocb *data){
 void setOut(off_t offset, struct aiocb *data){
 /*a classmate told me that I shouldnt be zeroing out stdout, 
  * I'm gonna look into why but for now it works*/
-    //set the fd to in for read
+    //set the fd to OUT macro for write
     data->aio_fildes = OUT;
     //set the bytes per block size
     data->aio_nbytes = offset;
