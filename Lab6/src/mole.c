8d40a4661b3b2174fa47d2ceb3175309dccfe00e
joshua.foster.208 <ffcleader@gmail.com>
2020-05-12 14:32:18 -0700

    Many small changes made several attempts at using a file to pass the current working directory but reverted to using method found online

--- Lab6/src/mole.c
+++ Lab6/src/mole.c
@@ -1,4 +1,4 @@
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -6,22 +6,26 @@
 #include <pwd.h>
 
 int main(int argc, char **argv){
-    char oldPath[FILENAME_MAX];
-    struct passwd *pw;
-    //grabs current working directory path
-    getcwd(oldPath, sizeof(oldPath));
-    pw=getpwent();
+//these warning messages are very annoying
+argc = argc;    
+
+//grabs current working directory path
+//Online claimed that it wasnt always safe to do it this way but was fine for this
+    uid_t uid = getuid();
+    struct passwd *pw = getpwuid(uid);
+        if (pw == NULL) {
+            fprintf(stderr, "Error:, Failed to reteive home directory\n");
+            exit(EXIT_FAILURE);
+        }
+//switch to the home directory
     chdir(pw->pw_dir);
 
     FILE * filePointer = fopen("lab6.log", "a");
-    
-            printf("bye%s\n", argv[0]);
-
     if (filePointer == NULL)        
         fprintf(stderr, "Error message: No log file created");
     else    
-        fprintf(filePointer, "Pop mole%s\n", argv[1]);
-        fclose(filePointer);
-        chdir(oldPath);
-    return 0;
+        fprintf(filePointer, "Pop mole%s\n", argv[0]);
+    fclose(filePointer);
+    chdir("/");
+    return (EXIT_SUCCESS);
 }
--- Lab6/src/spiritd.c
+++ Lab6/src/spiritd.c
@@ -10,198 +10,233 @@
 #include <sys/time.h>
 #include <time.h>
 #include <errno.h>
+#include <limits.h>
+#include <string.h>
 
+//variables updated by the signal handler on signal calls
 volatile sig_atomic_t sigTerm = 0;
 volatile sig_atomic_t sigUsr = -1;
 
+/*struct that stands for a mole
+ * id is the pid of the mole
+ * exists works as a bool flag that is used to know if the mole currently exists
+ */ 
 struct Mole{
     pid_t id;
-    int exists;//bool
+    int exists;
 };
 
 void sigSet();
-void sigHandler1(int sig);
-void sigHandler2(int sig);
+void sigHandler(int sig);
 void run();
-int setGpid();
+int  setGpid();
 void setDir();
 void setLim(struct rlimit* lim);
 void openDescriptor();
 void closeFileDescriptors();
 
-
+/*the main function of the program*/
 int main(int argc, char** argv) {
+//no parameters so used to remove the warning message in make
     argc = argc;
     argv[0] = argv[0];
-    printf("here\n");
     run();
     return (EXIT_SUCCESS);
 }
 
+/*Sets up the signal handler by calling signal on the three signal types
+ *USR1, USR2, TERM
+ */
 void sigSet(){
-    if(signal(SIGTERM, sigHandler1) == SIG_ERR){
+    if(signal(SIGTERM, sigHandler) == SIG_ERR){
         fprintf(stderr, "Error: Could not register SIGTERM!\n");
         exit(EXIT_FAILURE);
     }
-    if(signal(SIGUSR1, sigHandler2) == SIG_ERR){
+    if(signal(SIGUSR1, sigHandler) == SIG_ERR){
         fprintf(stderr, "Error: Could not register SIGUSR1!\n");
         exit(EXIT_FAILURE);
     }
-    if(signal(SIGUSR2, sigHandler2) == SIG_ERR){
+    if(signal(SIGUSR2, sigHandler) == SIG_ERR){
         fprintf(stderr, "Error: Could not register SIGUSR1!\n");
         exit(EXIT_FAILURE);
     }
 }
 
-void sigHandler2(int sig){
-   //if sigusr1 is caught update the sig_atomic_t sigusr to 1
-    if(sig == SIGUSR1)
+/*When a signal is caught update the associated sig_atomic_t variable accordingly*/
+void sigHandler(int sig){
+    if (sig == SIGTERM)
+        sigTerm = 1;
+    else if(sig == SIGUSR1)
         sigUsr = 0;
-   //if sigusr2 is caught update the sig_atomic_t sigusr to 2
     else if(sig == SIGUSR2)
         sigUsr = 1;
 }
 
-void sigHandler1(int sig){
-   //if sigterm is caught update the sig_atomic_t sigTerm to 1
-    if (sig == SIGTERM)
-       sigTerm = 1;
-}
-
 
 void run(){
-    pid_t childPid, gpid;
+    pid_t childPid;
     struct Mole mole1;
     struct Mole mole2;
     int i = 0, randNum = 0;
-    int errnum;    
+    char curDirect[PATH_MAX+1] = {"."};
+    char temp[PATH_MAX+1];
+
+//My original plan was to store the path in a file and access that from mole
+//then to delete the file at the end of this program. I couldn't think of a way to 
+//get the mole to find that file so i did something else, however after implementing
+//I just realized I could have just passed the path to the file in as a parameter 
+//or just passed the homedirectory in as a parameter instead lol oh well
+//char *homeDirect = getenv("HOME");
+ 
+//store the current working directory and add a '.' to the beginning and "/mole"
+//to the end for the call to execv later in the code
+    getcwd(temp, sizeof(curDirect));
+    strcat(curDirect, temp);
+    strcat(curDirect, "/mole");
+    printf("%s\n", curDirect);    
     
 //set the child creation mask to 0
     umask(0);//0000 or 0666
 
 //make a child process    
     childPid = fork();
-    if(childPid == -1){//if failed exit
+//if failed to fork exit    
+    if(childPid == -1){
        fprintf(stderr, "Error: Failed to to fork!\n");
-        exit(EXIT_FAILURE); 
+       exit(EXIT_FAILURE); 
     }
-    else if(childPid != 0){//if not the child close
-        printf("childPid =%d \n", childPid);
-     exit(EXIT_SUCCESS);   
+//if not the child exit successfully
+    else if(childPid != 0){
+       exit(EXIT_SUCCESS);   
     }   
     
-/*this is only called by the child process*/
-
+//This should only be called by the child process
+    
+//intialize params, params holds the parameters to be passed into execv
  char *params[2];
  params[0] = "0";
  params[1] = NULL;
  
-     //call sigSet here to 
+//call sigSet here to setup the signals in the child process
     sigSet();
     
 //sets it so rand is a different seed each time
     srand(time(NULL));
-    //set the group id/sid
-    gpid = setGpid();
-    //set directory to root
+    
+//set the group sid and exit if it fails
+    setGpid();
+    
+//set directory to root and exit if it fails
     setDir();
+    
 //create the limit struct and close all open file descriptors
+//tbh I need to look into this more I mostly got this part from what people did
+//online and from the text book they didn't explain very well
     struct rlimit limit;
     if (limit.rlim_max == RLIM_INFINITY){
         limit.rlim_max = 1024;
     }
+    
     for(i=0; i < (int)limit.rlim_max; i++)
         close(i);
-//opens the three file descriptors    
+    
+//opens the three file descriptors
     openDescriptor();
+    
+//make the moles id 1 so that they can enter the loop and enter the code correctly
+//make exists 0 as there are no moles as of now    
     mole1.id = 1;
     mole1.exists = 0;
     mole2.id = 1;
     mole2.exists = 0;    
-//while there has not been a sigterm signal
+    
+//Loop while there has not been a sigTerm signal
     while(!sigTerm){
-//pause and wait for a signal    
+        
+//pause and wait for a signal, not necessary but helps
     pause(); 
-//if sigUsr1 signal was received
+
+//prevents a child process that has been made to make a mole from entering and
+//making another process
     if(mole1.id != 0 || mole2.id != 0)
     {
+//if sigUsr1 signal was received
     if(sigUsr == 0){
-        if(mole1.exists == 1){//if mole is not a child
+        
+//if mole1 currently exists kill it and set exists back to 0
+        if(mole1.exists == 1){
           kill(mole1.id, SIGKILL);
           mole1.exists = 0;
         }
+        
+//randomly pick a numberfrom 1-2 for making a mole
         randNum = (rand() % 2) + 1;
+
+//if there is no mole1 and the random value was 1 make a new mole1
         if(mole1.exists == 0 && randNum == 1){
-                printf("im here mole1: %d\n",mole1.id);
             mole1.id = fork();
             mole1.exists = 1;
+            
+//if the child of the fork call execv            
             if(mole1.id == 0){
-                printf("umm mole1.id:%d\n", mole1.id);
             params[0] = "1";
-                if(execv("./mole", params) == -1){
-                    printf("error\n");
-                    errnum = errno;
-      fprintf(stderr, "Value of errno: %d\n", errno);
-      perror("Error printed by perror");
-      fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
-                }
+                if(execv(curDirect, params) == -1){
+                    fprintf(stderr, "error\n");
                 }
             }
+        }
+//if there is no mole1 and the random value was 2 make a new mole2
         else if (mole2.exists == 0 && randNum == 2){
             mole2.id = fork();
             mole2.exists = 1;
+            
+//if the child of the fork call execv            
             if(mole2.id == 0){
-                printf("umm mole2.id:%d\n", mole2.id);
             params[0] = "2";
-            execv("./mole", params);
-            if(execv("./mole", params) == -1){
-                    printf("error\n");
-                    errnum = errno;
-      fprintf(stderr, "Value of errno: %d\n", errno);
-      perror("Error printed by perror");
-      fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
+            if(execv(curDirect, params) == -1){
+                    fprintf(stderr, "error\n");
             }
                 }
             }
         }
+//if sigUsr2 signal was received
     else if(sigUsr == 1){
+//if mole2 currently exists kill it and set exists back to 0
             if(mole2.exists == 1){//if mole is not a child
-          kill(mole2.id, SIGKILL);
-          mole2.exists = 0;
+               kill(mole2.id, SIGKILL);
+               mole2.exists = 0;
         }
+        //randomly pick a numberfrom 1-2 for making a mole
         randNum = (rand() % 2) + 1;
+
+//if there is no mole1 and the random value was 1 make a new mole1
         if(mole1.exists == 0 && randNum == 1){
-                printf("im here mole1: %d\n",mole1.id);
             mole1.id = fork();
             mole1.exists = 1;
+            
+//if the child of the fork call execv            
             if(mole1.id == 0){
-                printf("umm mole1.id:%d\n", mole1.id);
             params[0] = "1";
-                 if(execv("./mole", params) == -1){
-                    printf("error\n");
-                    errnum = errno;
-      fprintf(stderr, "Value of errno: %d\n", errno);
-      perror("Error printed by perror");
-      fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
-            }
+                if(execv(curDirect, params) == -1){
+                    fprintf(stderr, "error\n");
                 }
             }
+        }
+//if there is no mole1 and the random value was 2 make a new mole2
         else if (mole2.exists == 0 && randNum == 2){
             mole2.id = fork();
             mole2.exists = 1;
+            
+//if the child of the fork call execv            
             if(mole2.id == 0){
-                printf("umm mole2.id:%d\n", mole1.id);
             params[0] = "2";
-             if(execv("./mole", params) == -1){
-                    printf("error\n");
-                    errnum = errno;
-      fprintf(stderr, "Value of errno: %d\n", errno);
-      perror("Error printed by perror");
-      fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
-            }
+            if(execv(curDirect, params) == -1){
+                    fprintf(stderr, "error\n");
                 }
             }
         }
+    }
+//reset the sigUsr variable
             sigUsr = -1;
     }
     }//end of while
@@ -212,6 +247,7 @@ void run(){
     //if parent do nothing and exit
 }
 
+/*setGpid sets the SID sends an error if fails*/
 int setGpid(){
     int gpid;
     if((gpid = setsid()) == -1){
@@ -221,7 +257,7 @@ int setGpid(){
     return gpid;
 }
 
-//sets the current working directory to be /
+/*sets the current working directory to be '/'c*/
 void setDir(){
  if(chdir("/") == -1){
      fprintf(stderr, "Error: Failed to set directory to '/'!\n");
@@ -229,6 +265,7 @@ void setDir(){
  }   
 }
 
+/*Sets the limit to the value passed in by the lim struct parameter*/
 void setLim(struct rlimit* lim){
    if(getrlimit(RLIMIT_NOFILE, lim) == -1){
      fprintf(stderr, "Error: Failed to set limits!\n");
@@ -236,15 +273,14 @@ void setLim(struct rlimit* lim){
  }    
 }
 
-void openDescriptor(){//attaches file descriptors 0,1,2 to /dev/null
+/*attaches file descriptors 0,1,2 to /dev/null*/
+void openDescriptor(){
     int f0,f1,f2;
     f0 = open("/dev/null", O_RDWR);//0
     f1 = dup(f0);//1
     f2 = dup(f0);//2
-    
+//just to remove the warning when calling make    
     f0 = f0;
     f1 = f1; 
     f2 = f2;
 }
-
-
